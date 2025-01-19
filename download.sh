25f3663785b0e7c5cc6e918588277a7dc1eb068a
chriswang0228 <114278435+chriswang0228@users.noreply.github.com>
2025-01-19 10:00:29 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,5 @@
 # Download dataset
-gdown 1zpYzxMtkPwzXVZaa2aueo0oX-XK9b8ji -O ckpt.zip
+gdown 1zpYzxMtkPwzXVZaa2aueo0oX-XK9b8ji -O ckpt.pt
 gdown 1w-Vk-ivkEZ_TKkRdy6ioFlTNHERWBSHO -O spalling_data.zip
 # Unzip the downloaded zip file
-unzip ./ckpt.zip
 unzip ./spalling_data.zip
